3616b95de892af62b7454f79bad315c3db657192
akifkurt <129757052+akifkurt@users.noreply.github.com>
2023-10-12 11:34:17 -0400

    ff addition table generator is added

--- headers/ff.h
+++ headers/ff.h
@@ -21,11 +21,47 @@ public:
     ff(int fieldSize);
 
     // Each of these will contain the finite field tables
-    std::vector<std::vector<uint8_t>> ff256;
-
+    std::vector<std::vector<uint8_t>> ff256_addition;
+    std::vector<std::vector<uint8_t>> ff256_multiplication;
     // @TODO: AKIF
-    std::vector<std::vector<uint8_t>> ff7;
-    std::vector<std::vector<uint8_t>> ff2;
+    std::vector<std::vector<uint8_t>> ff7_addition;
+    std::vector<std::vector<uint8_t>> ff7_multiplication;
+    std::vector<std::vector<uint8_t>> ff2_addition;
+    std::vector<std::vector<uint8_t>> ff2_multiplication;
+
+
+// Function to perform addition modulo the field size
+int addInField(uint8_t a, uint8_t b, int fieldSize) {
+    return (a + b) % fieldSize;
+}
+
+int main() {
+    int fieldSize = 16;
+
+    // Create a 2D vector to represent the addition table and initialize its dimensions
+    std::vector<std::vector<int>> ff_addition(fieldSize, std::vector<int>(fieldSize));
+
+    // Fill in the addition table
+    for (uint8_t i = 0; i < fieldSize; i++) {
+        for (uint8_t j = 0; j < fieldSize; j++) {
+            ff_addition[i][j] = addInField(i, j, fieldSize);
+        }
+    }
+
+    // Display the addition table
+    for (uint8_t i = 0; i < fieldSize; i++) {
+        for (uint8_t j = 0; j < fieldSize; j++) {
+            std::cout << ff_addition[i][j] << " ";
+        }
+        std::cout << std::endl;
+    }
+
+    return 0;
+}
+
+
+
+
 
     uint8_t add(uint8_t a, uint8_t b);
     uint8_t mutiply(uint8_t a, uint8_t b);
