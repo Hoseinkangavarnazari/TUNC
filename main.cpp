5aadfbcb711e8076c97c5f86c1ec79eaf7e432f7
akifkurt <makifkurt58@gmail.com>
2023-11-06 11:23:18 +0100

    mac_calculator is added, revision needed

--- main.cpp
+++ main.cpp
@@ -9,19 +9,26 @@
 #include "headers/cFunctions.h"
 #include "headers/packet.h"
 #include "headers/rlnc_decoder.h"
+#include "headers/hpacket.h"
+
+//hpacket macCalculator();
 
 int main()
 {
-
-    ff ff(256);
-
-    int symbolSize = 4;
-    int generationSize = 5;
-
+    std::vector<std::vector<uint8_t>> publickeyset = {{1,2,3},{2,3,4},{3,4,5},{4,5,6},{5,6,7}};
+    std::vector<std::vector<uint8_t>> hcodedSymbol = {{2,4},{1,3},{6,7}};          
+    //hpacket packettemp ;
+      ff ff(256);
+    
+    int symbolSize = 2;
+    int generationSize = 6;
+    //std::vector<std::vector<uint8_t>> test_mac;
+    hpacket hp( hcodedSymbol, publickeyset, publickeyset.size());
+    //test_mac = macCalculator(hcodedSymbol, publickeyset, 5);
     FieldSize fieldSize = BinaryC;
     int dataSizeInBytes = generationSize * symbolSize;
     std::vector<uint8_t> sampleData = randomDataGenerator(dataSizeInBytes, fieldSize);
-
+    std::vector<std::vector<uint8_t>>test_mac = hp.macCalculator(hcodedSymbol,publickeyset,publickeyset.size());
     rlnc_encoder encoder(generationSize, symbolSize, fieldSize);
     rlnc_decoder decoder(generationSize, symbolSize, fieldSize);
 
@@ -33,25 +40,25 @@ int main()
     std::vector<packet> allPackets;
 
     int packetCount = 0;
-    while (packetCount != generationSize)
-    {   
-        // create a coded packet
-        // First, generate a random coefficient
-        std::vector<uint8_t> tempCoeff;
-        encoder.randomCoeffGenerator(tempCoeff);
-        // Second, encode using the coeff
-        std::vector<uint8_t> codedSybmols = encoder.encode_rlnc(tempCoeff);
-        // Third, create a packet object
-        packet p(tempCoeff, codedSybmols,packetCount);
+    //    while (packetCount != generationSize)
+    //  {   
+// create a coded packet
+// First, generate a random coefficient
+    //    std::vector<uint8_t> tempCoeff;
+      //  encoder.randomCoeffGenerator(tempCoeff);
+// Second, encode using the coeff
+       // std::vector<uint8_t> codedSybmols = encoder.encode_rlnc(tempCoeff);
+// Third, create a packet object
+        //packet p(tempCoeff, codedSybmols,packetCount);
 
 
         // give the packet to the decoder 
-        allPackets.push_back(p);
-        decoder.consumeCodedPacket(p);
-        packetCount++;
-    }
+        //allPackets.push_back(p);
+        //decoder.consumeCodedPacket(p);
+        //packetCount++;
+    //}
 
-    decoder.decode();
+    //decoder.decode();
 
 
     
