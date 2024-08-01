f09085668df6e2ac3824ff9481ccd97e5c378cf7
akifkurt <makifkurt58@gmail.com>
2024-08-01 15:50:40 +0200

    updated the cache

--- CMakeFiles/runner.out.dir/headers/src/mac_calculator.cpp.o.d
+++
@@ -1,3 +0,0 @@
-CMakeFiles/runner.out.dir/headers/src/mac_calculator.cpp.o: \
- /home/akif/Documents/TUNC/TUNC/headers/src/mac_calculator.cpp \
- /usr/include/stdc-predef.h
--- CMakeFiles/runner.out.dir/headers/src/sign_calculator.cpp.o.d
+++
@@ -1,3 +0,0 @@
-CMakeFiles/runner.out.dir/headers/src/sign_calculator.cpp.o: \
- /home/akif/Documents/TUNC/TUNC/headers/src/sign_calculator.cpp \
- /usr/include/stdc-predef.h
